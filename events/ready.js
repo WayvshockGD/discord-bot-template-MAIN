1e51c1908d40c74470e34963036ffb5a37354319
vipgabe09267 <68444470+vipgabe09267@users.noreply.github.com>
2020-08-11 18:23:19 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("Officer Joe Reporting For Duty!", { type: "" });
+    bot.user.setActivity("Made By: Vipgabe09267#4386's bot builder", { type: "" });
 }
